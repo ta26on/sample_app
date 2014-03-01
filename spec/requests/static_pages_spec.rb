f81971e0a31ee4e55874d9b860e77ba0d6390bf7
ta26on <ta26on@gmail.com>
2014-03-01 16:38:33 +0900

    contact ページ追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -29,13 +29,24 @@ describe "StaticPages" do
 	describe "About page" do
 		it "should have the content 'About Us" do
 			visit '/static_pages/about'
-			page.should have_content('About Us')
+			expect(page).to have_content('About Us')
 		end
 
 		it "should have the right title" do
 			visit '/static_pages/about'
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 		end
+	end
 
+	describe "Contact" do
+		it "should have the content 'Contact" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+
+		it "should have the right title 'Contact" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+		end
 	end
 end
