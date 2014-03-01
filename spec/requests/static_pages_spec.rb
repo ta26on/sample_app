eb0f997879ebbd6d0df109d3e17fce70f2c90dca
ta26on <ta26on@gmail.com>
2014-03-01 15:43:21 +0900

    タイトル入力

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,15 +4,26 @@ describe "StaticPages" do
 	describe "Home page" do
 		it "should have the content 'Sample App" do
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			expect(page).to have_content('Sample App')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
 		end
 	end
 
 	describe "Help page" do
 		it "should have the content 'Help" do
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			expect(page).to have_content('Help')
 		end
+
+		it "should have the right title" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		end
+
 	end		
 
 	describe "About page" do
@@ -20,5 +31,11 @@ describe "StaticPages" do
 			visit '/static_pages/about'
 			page.should have_content('About Us')
 		end
+
+		it "should have the right title" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+		end
+
 	end
 end
