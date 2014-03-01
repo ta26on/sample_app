a8854f8da6cd11baab6e2a3ff47cadaaa58366f2
ta26on <ta26on@gmail.com>
2014-03-01 11:00:49 +0900

    add page: about

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,4 +14,11 @@ describe "StaticPages" do
 			page.should have_content('Help')
 		end
 	end		
+
+	describe "About page" do
+		it "should have the content 'About Us" do
+			visit '/static_pages/about'
+			page.should have_content('About Us')
+		end
+	end
 end
