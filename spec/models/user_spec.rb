b7d873e330e1955763d8b42d5672778f13507a8b
ta26on <ta26on@gmail.com>
2014-03-04 01:28:03 +0900

    ~6.4 暗号化されたPasswordを扱う 実際にdbにいれて確認できたところまで（consoleで）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,21 +14,29 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
   it { should be_valid }
 
+  #
+  # name 関連
+  #
   describe "when name is no present" do
   	before { @user.name = " " }
   	it { should_not be_valid }
   end
 
-  describe "when email is not present" do
-  	before { @user.email = " " }
-  	it { should_not be_valid }  	
-  end
 
   describe "when name is too long" do
   	before { @user.name = "a" * 51 }
   	it { should_not be_valid } 
+  end
+
+  #
+  # email 関連
+  # 
+  describe "when email is not present" do
+  	before { @user.email = " " }
+  	it { should_not be_valid }  	
   end
 
   describe "when email format is invalid" do
@@ -61,6 +69,14 @@ describe User do
   	it { should_not be_valid }
   end
 
+  #
+  # Password関連
+  #
+  describe "with a password that's too short" do
+  	before { @user.password = @user.password_confirmation = "a"*5 }
+  	it { should be_invalid }
+  end
+
   describe "when password is not present" do
   	before do
   		@user = User.new(name: "Example User", email: "user@example.com", 
@@ -72,6 +88,22 @@ describe User do
 
   describe "when password doesn't match confirmation" do
   	before { @user.password_confirmation = "mismatch" }
-  	it {should_not be_valid }
+  	it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+  	before { @user.save } # これで find_by が使えるようになる
+  	let(:found_user){ User.find_by(email: @user.email) }
+
+  	describe "with valid password" do
+  		it { should eq found_user.authenticate(@user.password) }
+  	end
+
+  	describe "with invalid password" do
+  		let(:user_for_invalid_password){ found_user.authenticate("invalid") }
+
+  		it { should_not eq user_for_invalid_password }
+  		specify { expect(user_for_invalid_password).to be_false } # specify は it と同じ．英語として自然な方を使う．
+  	end
   end
 end
