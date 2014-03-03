f8be600f46e8dae53b5946c4c8d2ddc35c124ffa
ta26on <ta26on@gmail.com>
2014-03-03 23:16:37 +0900

    email アドレスの format validation 追加

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe User do
+  before { @user = User.new(name: "Example User", email: "user@example.com")}
+  subject { @user }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+
+  describe "when name is no present" do
+  	before { @user.name = " " }
+  	it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+  	before { @user.email = " " }
+  	it { should_not be_valid }  	
+  end
+
+  describe "when name is too long" do
+  	before { @user.name = "a" * 51 }
+  	it { should_not be_valid } 
+  end
+
+  describe "when email format is invalid" do
+  	it "should be invalid" do
+  		addresses = %W[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+
+  		addresses.each do |invalid_address|
+  			@user.email = invalid_address
+  			expect(@user).not_to be_valid
+  		end
+  	end
+  end
+
+  describe "when email format is valid" do
+  	it "should be valid" do
+  		addresses = %W[user@foo.COM a_US-ER@f.b.org frest.lst@foo.jp a+b@baz.cn ]
+  		addresses.each do |valid_address|
+  			@user.email = valid_address
+  			expect(@user).to be_valid
+  		end
+  	end
+  end
+
+end
