64b515a8254f973e992b2f4a4644073d253b6c67
ta26on <ta26on@gmail.com>
2014-03-04 01:49:21 +0900

    大文字小文字の テスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -69,6 +69,16 @@ describe User do
   	it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+  	let(:mixed_case_email){ "UpUp@downcase.COm" }
+
+  	it "should be saved as all lower-case" do
+  		@user.email = mixed_case_email
+  		@user.save
+  		expect(@user.reload.email).to eq mixed_case_email.downcase
+  	end
+  end
+
   #
   # Password関連
   #
