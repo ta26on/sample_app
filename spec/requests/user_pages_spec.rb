747a2defde84fa0e1f14ee7a0918b9e481de5355
ta26on <ta26on@gmail.com>
2014-03-04 10:36:37 +0900

    gem 追加: factory_girl_rails

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,6 +6,15 @@ describe "UserPages" do
 	describe "signup page" do
 		before { visit signup_path }
 		it { should have_content('Sign up')}
-  	it { should have_title(full_title('Sign up')) }
-  end
+		it { should have_title(full_title('Sign up')) }
+	end
+
+	describe "prifile page" do
+		# ユーザー変数を作成するためのコードに置き換える
+		before { visit user_path(user) }
+
+		it { should have_content(user.name) }
+		it { should have_title(user.name) }
+	end
+
 end
